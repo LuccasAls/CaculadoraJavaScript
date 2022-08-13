97fc2c79af4a51e5e5cb7365f5124f103231d5a3
Luccas <luccasalves.rmr@gmail.com>
2022-08-13 19:22:29 -0300

    Calculadora

--- Calculadora/script.js
+++ Calculadora/script.js
@@ -2,60 +2,100 @@ const calculator = document.querySelector('.container')
 const keys = document.querySelector('.containerValue')
 
 
+const display = document.querySelector('input[type="text"]')
+
+
+  
 keys.addEventListener('click', e => {
     if (e.target.matches('button')) {
-        const key = e.target // elemento html
-        const action = key.dataset.action // pegando os elementos que tem data action, se não tiver retorna undefined
-       
-    if (!action) { // se não tiver retorna "!" (falso)
-       // console.log('number key!')
-    }
-
-    if (action === 'add' ||
-        action === 'subtract' ||
-        action === 'multiply' ||
-        action === 'divide'
-      ) {
-       // console.log('operator key!')
+    const key = e.target
+    const action = key.dataset.action
+    const keyContent = key.textContent
+    const displayedNum = display.value
+    
+    if (!action) {
+      if (displayedNum == '0') {
+        display.value = keyContent 
+      } else {
+        display.value = displayedNum + keyContent
       }
-
-    if( action === 'decimal'){
-       // console.log('Decimal key')
     }
-    if( action === 'clear'){
-       // console.log('Clear key')
-    }
-    if (action === 'equal'){
-        //console.log('Equal key')
-    }
- }
-})
 
-const numberPainel = document.querySelector('#filedNumber')
-
-keys.addEventListener('click', e => {
-    if( e.target.matches('button')){
-        const key = e.target
-        const action = key.dataset.action
-        const keyContent = key.textContent // retorna o text buttom
-        const painelNum = numberPainel.value // retorna o text do input
-
-
-        if (!action) {
-            if (painelNum === '0') {
-                numberPainel.value = keyContent
-            } else {
-                numberPainel.value = painelNum + keyContent
-            }
-        }
-
-        if (action === 'decimal') {
-            numberPainel.value = painelNum + '.' ;
-            
+      if (action === 'add') {
+        display.value = displayedNum + '+'
+      
+        if (displayedNum.substr(-1) === '+') {
+          display.value = displayedNum
         }
         
-    }
+      }
+      if (action === 'subtract') {
+        display.value = displayedNum + '-'
+
+        if (displayedNum.substr(-1) === '-') {
+          display.value = displayedNum
+        }
+        
+      }
+      if (action === 'multiply') {
+        display.value = displayedNum + '*'
+        if (displayedNum.substr(-1) === '*') {
+          display.value = displayedNum
+        }
+        
+      }
+      if (action === 'divide') {
+        display.value = displayedNum + '/'
+        if (displayedNum.substr(-1) === '/') {
+          display.value = displayedNum
+        }
+        
+      }
+      
+
+      if (action === 'decimal') {
+        display.value = displayedNum + '.'
+        if (displayedNum.includes('.')) {
+          display.value = displayedNum
+        }
+      }
+      
+      if (action === 'clear') {
+        display.value = '0'
+      }
+      
+
+  
+  }
 })
+
+
+const equal = document.querySelector('.equal')
+
+equal.onclick = ()=> {
+  display.value = eval(display.value)
+}
+
+
+
+
+
+
+const del = document.querySelector('.del')
+
+del.onclick = ()=> {
+    const displayNum = display.value
+
+    display.value = displayNum.substring(0, displayNum.length -1)
+
+    if (display.value === '' ) {
+      display.value = ' 0'
+      
+     }
+    
+
+}
+
 
 
 
